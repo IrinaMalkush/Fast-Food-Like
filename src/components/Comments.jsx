759399299cde20afabf75d64eadaae87f81d1907
Irina <irinamalkush@gmail.com>
2020-04-20 23:09:13 +0300

    add inDevelopmentStage page

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./InDevStage.css";
 
 export function Comments() {
-    return(
+  return (
+    <div className="in-dev-stage">
+      <div className="in-dev-stage-container">
         <p>Страница находится в разработке</p>
-    )
+        <img
+          src="/img/inwork.jpg"
+          alt="the-page-is-in-development-stage"
+          className="in-dev-stage-image"
+        ></img>
+      </div>
+    </div>
+  );
 }
